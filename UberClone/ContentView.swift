af7fe0e3b771d690512db6739790d6eb73f7f1ba
naolgithub <naolcovered@gmail.com>
2024-11-11 14:06:10 +0300

    Initial Commit

---
+++ UberClone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UberClone
+//
+//  Created by Naol Abera on 11/11/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UberClone/UberCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberCloneApp.swift
+//  UberClone
+//
+//  Created by Naol Abera on 11/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct UberCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
